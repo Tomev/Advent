0dd6d90a3579af01c16e91d5aa669edd9c1b5b05
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-04 12:52:11 +0100

    First day of advent completed.

---
+++ Day1/Day1.1.py
@@ -0,0 +1,30 @@
+file_read = open("input.txt", "r")
+
+line = file_read.readline()
+
+freq = 0
+frequencies_achieved = [freq]
+was_first_double_frequency_found = False
+first_double_frequency = 0
+
+while line:
+    number = int(line[1:])
+    if line[0] == '-':
+        freq -= int(number)
+    else:
+        freq += int(number)
+
+    if frequencies_achieved.__contains__(freq) and not was_first_double_frequency_found:
+        was_first_double_frequency_found = True
+        first_double_frequency = freq
+
+    frequencies_achieved.append(freq)
+
+    line = file_read.readline()
+
+    print(f'Current freq = {freq}')
+
+print(f'First dobule frequency found was {first_double_frequency}.')
+print(frequencies_achieved)
+
+
