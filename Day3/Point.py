cd7b5f29101a5a6a07d6979a909d39dcf734f9e0
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-05 10:11:43 +0100

    Cleaned Day3.1.

---
+++ Day3/Point.py
@@ -0,0 +1,13 @@
+class Point:
+    x = 0
+    y = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __ref__(self):
+        return self.__str__()
