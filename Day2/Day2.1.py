4f75f793747f6b3de0dca11c40dd1ec95d9e35b1
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-05 08:48:15 +0100

    Added solution to day's 2 1st puzzle.

--- Day2/Day2.1.py
+++ Day2/Day2.1.py
@@ -1,18 +1,29 @@
+def has_letter_that_occurs_exactly_n_times(string, n):
+    char_set = set(string)
+
+    for char in char_set:
+        if string.count(char) == n:
+            return True
+
+    return False
+
+
 file_read = open("input.txt", "r")
 
 line = file_read.readline()
 
-freq = 0
+ids_with_letters_occurring_2_times = 0
+ids_with_letters_occurring_3_times = 0
 
 while line:
-    number = int(line[1:])
-    if line[0] == '-':
-        freq -= int(number)
-    else:
-        freq += int(number)
+
+    if has_letter_that_occurs_exactly_n_times(line, 2):
+        ids_with_letters_occurring_2_times += 1
+    if has_letter_that_occurs_exactly_n_times(line, 3):
+        ids_with_letters_occurring_3_times += 1
 
     line = file_read.readline()
 
-    print(f'Current freq = {freq}')
+print(f'Checksum: {ids_with_letters_occurring_2_times * ids_with_letters_occurring_3_times}.')
 
 
