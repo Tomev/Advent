aa4f6c98593a81f88dde9d0215d787f43b7ac76c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-05 13:33:10 +0100

    Refactored day 5 solution.

--- Day5/Day5.1.py
+++ Day5/Day5.1.py
@@ -1,25 +1,11 @@
+from Day5.polymer_reduction import reduce_polymer
+
 file_read = open("input.txt", "r")
 
 polymer = file_read.readline()
 polymer = polymer[:len(polymer) - 1]  # remove new line char
 
-i = 0
-was_polymer_reduced = True
-
-while True:
-
-    if abs(ord(polymer[i]) - ord(polymer[i + 1])) == 32:
-        polymer = polymer[:i] + polymer[i + 2:]
-        was_polymer_reduced = True
-    else:
-        i += 1
-
-        if i == len(polymer) - 1 and not was_polymer_reduced:
-            break
-
-    if i == len(polymer) - 1:
-        i = 0
-        was_polymer_reduced = False
+polymer = reduce_polymer(polymer)
 
 print(polymer)
 print(f'Resulting polymer contains {len(polymer)} units.')
---
+++ Day5/polymer_reduction.py
@@ -0,0 +1,20 @@
+def reduce_polymer(poly):
+    i = 0
+    was_polymer_reduced = True
+
+    while True:
+
+        if abs(ord(poly[i]) - ord(poly[i + 1])) == 32:
+            poly = poly[:i] + poly[i + 2:]
+            was_polymer_reduced = True
+        else:
+            i += 1
+
+            if i == len(poly) - 1 and not was_polymer_reduced:
+                break
+
+        if i == len(poly) - 1:
+            i = 0
+            was_polymer_reduced = False
+
+    return poly
