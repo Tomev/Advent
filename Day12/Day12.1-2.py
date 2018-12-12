68a2b78ec8d275168f958c14fedc803764fccd3e
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-12 21:57:53 +0100

    Added solution to day 12.

--- Day12/Day12.1-2.py
+++ Day12/Day12.1-2.py
@@ -1,5 +1,5 @@
 file_read = open("test.txt", "r")
-# file_read = open("input.txt", "r")
+file_read = open("input.txt", "r")
 
 # Store data in dict
 
@@ -10,7 +10,7 @@ line = file_read.readline()
 state = line.split('initial state: ')[1]
 state = state[:-1]
 
-line = file_read.readline() # Omit empty line
+line = file_read.readline()  # Omit empty line
 line = file_read.readline()
 
 while line:
@@ -19,23 +19,26 @@ while line:
     line = file_read.readline()
 
 iterations_num = 20
+iterations_num = 50000000000  # Puzzle part 2
 plants_num = 0
 start_offset = 0
 
-for i in range(1, iterations_num + 2):
+for i in range(1, iterations_num + 1):
 
     plants_num += state.count('#')
 
-    if state[:2].__contains__('#'):
-        state = '..' + state
-        start_offset += 2
+    if i % 1000000 == 0:
+        print(f'iteration: {i}')
+
+    state = '.....' + state
+    start_offset += 3
 
     if state[-3:].__contains__('#'):
-        state += '...'
+        state += '....'
 
-    print(f'{i - 1} state: {state}')
+    # print(f'{i - 1} state: {state}')
 
-    new_state = '..'
+    new_state = ''
 
     for j in range(2, len(state) - 2):
 
@@ -47,9 +50,14 @@ for i in range(1, iterations_num + 2):
         if rule_key in rules:
             new_state += rules[rule_key]
         else:
-            #new_state += state[j]
             new_state += '.'
 
     state = new_state
 
-print(start_offset)
+plants_positions_sum = 0
+
+for i in range(0, len(state)):
+    if state[i] == '#':
+        plants_positions_sum += i - start_offset
+
+print(f'The sum of the numbers of all pots which contain a plant equals {plants_positions_sum}.')
